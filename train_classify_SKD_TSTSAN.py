7c749f0e7182168b34a398fc27ab32589e87069f
pinellia0000 <pinellia0000@163.com>
2025-09-24 11:19:34 +0800

    Update train_classify_SKD_TSTSAN.py

--- train_classify_SKD_TSTSAN.py
+++ train_classify_SKD_TSTSAN.py
@@ -26,11 +26,11 @@ if __name__ == '__main__':
 
     parser.add_argument('--Aug_alpha', type=float, default=2)
 
-    parser.add_argument('--part_Subjects', type=list, default=[], help='部分训练实例')
-    parser.add_argument('--part_total_gt', type=list, default=[], help='部分训练实例')
-    parser.add_argument('--part_total_pred', type=list, default=[], help='部分训练实例')
-    parser.add_argument('--part_best_total_pred', type=list, default=[], help='部分训练实例')
-    parser.add_argument('--part_all_accuracy_dict', type=dict, default={}, help='部分训练实例')
+    parser.add_argument('--part_Subjects', type=str, default='[]', help='部分训练实例')
+    parser.add_argument('--part_total_gt', type=str, default='[]', help='部分训练实例')
+    parser.add_argument('--part_total_pred', type=str, default='[]', help='部分训练实例')
+    parser.add_argument('--part_best_total_pred', type=str, default='[]', help='部分训练实例')
+    parser.add_argument('--part_all_accuracy_dict', type=str, default='{}', help='部分训练实例')
 
     config = parser.parse_args()
 
