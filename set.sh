ae1ffdbf837f3f3e7217c1b6dcb8c808689e4af7
pinellia0000 <pinellia0000@163.com>
2026-03-01 16:51:00 +0800

    更新

---
+++ set.sh
@@ -0,0 +1,18 @@
+# 不同环境需要修改
+# source /root/miniconda3/bin/activate newCondaEnvironment
+pip install --upgrade pip
+pip list --outdated
+pip install --upgrade setuptools
+pip list
+python --version
+pip --version
+
+# 检测目前使用的python环境
+which python
+which pip
+
+# 下载项目代码并安装依赖
+rm MERecognition -rf
+git clone https://github.com/Pinellia0000/MERecognition.git
+# # 安装依赖
+pip install -r /root/MERecognition/requirements.txt
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+# sh /root/MERecognition/start.sh
 # 输出北京时间
 TZ="Asia/Shanghai" date
 
@@ -19,21 +20,3 @@ nvcc -V
 conda update -y -n base -c conda-forge conda
 # Create New Conda Environment and Use Conda Channel
 conda create -y -n newCondaEnvironment python=3.10
-# 不同环境需要修改
-source /root/miniconda3/bin/activate newCondaEnvironment
-pip install --upgrade pip
-pip list --outdated
-pip install --upgrade setuptools
-pip list
-python --version
-pip --version
-
-# 检测目前使用的python环境
-which python
-which pip
-
-# 下载项目代码并安装依赖
-rm MERecognition -rf
-git clone https://github.com/Pinellia0000/MERecognition.git
-# # 安装依赖
-pip install -r /root/MERecognition/requirements.txt
